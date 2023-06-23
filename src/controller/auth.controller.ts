343f8b0207546eac93eb089ba88de81e49d78283
siva <ksivakumar0211@gmail.com>
2023-06-23 17:50:34 +0530

    login files are added.done by kalaimathi

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,6 +1,6 @@
 
 
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger/dist/decorators/api-bearer.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { AuthService } from 'src/auth/services/auth.service';
@@ -12,10 +12,10 @@ export class AuthController {
 	constructor(private authService: AuthService,
    ) { }
 
-	// @Get('getUserAuthentication/:password/:username')
-	// getUserAuthentication(@Param('password') password: string, @Param('username') username: string, ): Promise<any> {
-	// 	return this.AuthService.getUserAuthentication(password, username);
-	// }
+	@Get('getUserAuthentication/:password/:username')
+	getUserAuthentication(@Param('password') password: string, @Param('username') username: string, ): Promise<any> {
+		return this.authService.getUserAuthentication(password, username);
+	}
   @Post('login')
 async create(@Body() rnUsersDTO:RnUsersDTO ): Promise<any> {
 	const user = await this.authService.validateUser(rnUsersDTO.password,rnUsersDTO. email);
